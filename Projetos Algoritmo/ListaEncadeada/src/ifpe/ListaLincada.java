492bf9ea61e3ac7140417c2c7d7202ab90bc0957
LuizBuarqueDev <luizgabrielbuarque>
2023-11-07 17:47:57 -0300

    a

--- Projetos Algoritmo/ListaEncadeada/src/ifpe/ListaLincada.java
+++ Projetos Algoritmo/ListaEncadeada/src/ifpe/ListaLincada.java
@@ -2,29 +2,27 @@ package ifpe;
 
 public class ListaLincada {
 
-	private No head;
-	
-	public void add(String dado) {
-		// TODO Auto-generated method stub
-		No NovoNo = new No("1");
-		if (head == null) {
-			head = NovoNo;
-		}else {
-			No noTemp = head;
-			while(noTemp != null) {
-				noTemp = noTemp.next;
-			}
-			noTemp.next = NovoNo;
-		}
-	}
-	public void print() {
-		No noTemp = head;
-		while(noTemp != null) {
-			System.out.println(noTemp.dado + " ");	
-			noTemp = noTemp.next;
-		}
-		System.out.println();
-		
-		
-	}
+    private No head;
+
+    public void add(String dado) {
+        No novoNo = new No(dado);
+        if (head == null) {
+            head = novoNo;
+        } else {
+            No noTemp = head;
+            while (noTemp.next != null) {
+                noTemp = noTemp.next;
+            }
+            noTemp.next = novoNo;
+        }
+    }
+
+    public void print() {
+        No noTemp = head;
+        while (noTemp != null) {
+            System.out.print(noTemp.dado + " ");
+            noTemp = noTemp.next;
+        }
+        System.out.println();
+    }
 }
