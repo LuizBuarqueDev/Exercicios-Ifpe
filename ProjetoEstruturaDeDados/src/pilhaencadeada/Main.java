511024e45b19e28c843c0ccebe423527fcff479b
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2023-11-27 17:46:30 -0300

    Começo do main

--- ProjetoEstruturaDeDados/src/pilhaencadeada/Main.java
+++ ProjetoEstruturaDeDados/src/pilhaencadeada/Main.java
@@ -4,30 +4,34 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("Qualquer numero invalido resultara na finalização do codigo");
 		PilhaEncadeada pilhaEncadeada = new PilhaEncadeada();
 		Scanner scanner = new Scanner(System.in);
-		System.out.println(
-				"\n[1]-enqueue\n[2]-equeue\n[3]-size\n[4]-isEmpty\n[5]-front\n Qualquer numero invalido resultara na finalização do codigo");
 		while (true) {
-
+			System.out.println("[1]-push(x) ; [2]-pop() ; [3]-size() ; [4]-top() ; [5]-isEmpty() ; [6]-print()");
 			String entrada = scanner.nextLine();
-			if (entrada == "1") {
+			
+			if (entrada.equals("1")) {
+				System.out.print("Push: Elemento:");
+				pilhaEncadeada.push(scanner.nextLine());
 
-			} else if (entrada == "2") {
+			} else if (entrada.equals("2")) {
+				System.out.println("Pop: " + pilhaEncadeada.pop());
 
-			} else if (entrada == "3") {
+			} else if (entrada.equals("3")) {
 
-			} else if (entrada == "4") {
+			} else if (entrada.equals("4")) {
 
-			} else if (entrada == "5") {
+			} else if (entrada.equals("5")) {
+
+			} else if (entrada.equals("6")) {
+				pilhaEncadeada.print();
 
 			} else {
 				System.out.println("Fim da operação\n Fila: ");
 				pilhaEncadeada.print();
 				break;
 			}
-
 		}
 	}
-
 }
